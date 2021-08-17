529ffdbccc66571d36b3a1beba2532c3adb81e4d
Vitaly Petrov <Fainorthegreat3@gmail.com>
2021-08-17 16:28:22 +0300

    Delete

--- js-diploma/src/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import photoReducer from './reducers/photoReducer';
-
-export default combineReducers({
-    photoReducer: photoReducer,
-    routing: routerReducer
-});
--- test/js/chart/types/helpers/helpers.easing.d.ts
+++
@@ -1,5 +0,0 @@
-import { EasingFunction } from '../core/interfaces';
-
-export type EasingFunctionSignature = (t: number) => number;
-
-export const easingEffects: Record<EasingFunction, EasingFunctionSignature>;
